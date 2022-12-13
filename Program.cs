6307340bf39969a19de8b48f8e0e9ae331dcf1b6
Gilberto Espinosa <gilbertosilvaespinosa@gmail.com>
2022-12-13 09:59:26 -0300

    Começando a colar peças no tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using tabuleiro;
-using xadrez_console;
+using xadrez;
 
-namespace Xadrez_console
+namespace xadrez_console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, tabuleiro.Enums.Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, tabuleiro.Enums.Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab,tabuleiro.Enums.Cor.Preta), new Posicao(2, 4));
 
             Tela.imprimirTabuleiro(tab);
 
